5364179ec4c29726f666ab53f383ae8bddeda05f
Yuto Horiuchi <hotz07ggl@gmail.com>
2014-03-29 05:58:26 +0900

    p194
    It looks like beautiful code after I was coding.

--- p194/p194/main.c
+++ p194/p194/main.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-
 int main(void)
 {
     int score = 0;
-    
     do{
         if (score != 0)
         printf("点数は0~100の間の値を入力してください\n");
         printf("点数を入力してください\n");
-        scanf("%d", &score);
-    }while (score <0||score>100);
+        scanf("%d",&score);
+    }while (score <0||score >100);
     printf("入力された点数%d\n",score);
     return 0;
+    
 }
