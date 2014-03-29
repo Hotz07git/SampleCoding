e3b6da489548df6ec69a384400f08d88fd9cf516
Yuto Horiuchi <hotz07ggl@gmail.com>
2014-03-29 09:05:15 +0900

    p206
    This is an example about coding.

---
+++ p206/p206/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int sum(int); /*プロトタイプ宣言*/
+int main(void)
+{
+    sum(50); /*50を渡している*/
+    return 0;
+}
+int sum(int max)
+{
+    printf("%d\n", (1 + max) * max / 2);
+    return 0;
+}
