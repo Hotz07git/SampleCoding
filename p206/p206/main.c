0e6c7e3a0c2bb687e7f91090382fc85152bc59b2
Yuto Horiuchi <hotz07ggl@gmail.com>
2014-03-29 09:11:12 +0900

    p206
    The code is more beautiful than ever before.

--- p206/p206/main.c
+++ p206/p206/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-int sum(int); /*プロトタイプ宣言*/
+int sum(int);
 int main(void)
 {
-    sum(50); /*50を渡している*/
+    sum(50);
     return 0;
 }
 int sum(int max)
 {
-    printf("%d\n", (1 + max) * max / 2);
+    printf("%d\n",(1+max)*max/2);
     return 0;
 }
